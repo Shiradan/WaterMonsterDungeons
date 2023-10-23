edad0a99a73c0c7621eb3f27b7d9ebf788410973
Shiradan <qiantianwen89@gmail.com>
2023-10-23 15:30:25 +0800

    Update sound_manger.gd

--- autoload/sound_manger.gd
+++ autoload/sound_manger.gd
@@ -3,13 +3,14 @@ extends Node
 @onready var ovani_player = $OvaniPlayer
 @onready var audio_stream_player = $AudioStreamPlayer
 var login_page_bgm:OvaniSong=load("res://music/Wonderful (RT 5.334)/wonderful.tres")
-var main_bgm=load("res://music/Relaxed (RT 6.314)/relaxed.tres")
+var main_bgm:OvaniSong=load("res://music/Relaxed (RT 6.314)/relaxed.tres")
+var mouse_click=load("res://music/Click Glass Dry.wav")
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		var mouseEvent = event as InputEventMouseButton
 		if mouseEvent.button_index == MOUSE_BUTTON_LEFT and mouseEvent.pressed:
-			audio_stream_player.stream=load("res://music/Click Glass Dry.wav")
+			audio_stream_player.stream=mouse_click
 			audio_stream_player.play()
 
 func play_login_page_bgm():
