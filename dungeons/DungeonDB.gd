285248ab1d630075d0ceb458a4d56daee6078c6e
Shiradan <qiantianwen89@gmail.com>
2023-11-15 10:26:48 +0800

    dungeon DB template

--- dungeons/DungeonDB.gd
+++ dungeons/DungeonDB.gd
@@ -2,8 +2,16 @@ extends TextDatabase
 
 func _initialize():
 	#地城索引
-	add_mandatory_property("Skill_Index",TYPE_FLOAT)
+	add_mandatory_property("Dungeon_Index",TYPE_FLOAT)
 	#中文名——必要
 	add_mandatory_property("cn_name",TYPE_STRING)
 	#地城描述——必要
 	add_mandatory_property("Desc",TYPE_STRING)
+	#各层战斗配置
+	add_mandatory_property("Levels",TYPE_ARRAY)
+	#胜利文本
+	add_valid_property("victory_desc",TYPE_STRING)
+	#失败文本
+	add_valid_property("defeat_desc",TYPE_STRING)
+	#怪物平均等级
+	add_valid_property("CR",TYPE_FLOAT)
